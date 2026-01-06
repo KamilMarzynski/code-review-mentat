c79d6a5be50e0ac953625bdaf82bd0863bbc4a1c
KamilMarzynski <kamil.marzynski@gmail.com>
2026-01-06 15:08:14 +0100

    Remove unused class

--- src/review/review-service.ts
+++ src/review/review-service.ts
@@ -13,43 +13,6 @@ export class ReviewService {
     this.graph = this.buildGraph();
   }
 
-  private buildGraph() {
-    return new StateGraph(reviewState)
-      .addNode('contextSearchCall', (state: ReviewState, config: LangGraphRunnableConfig) => this.contextGatherer.gather(state, config))
-      .addNode('reviewCall', (state: ReviewState, config: LangGraphRunnableConfig) => this.codeReviewer.review(state,config))
-      .addEdge(START, 'contextSearchCall')
-      .addEdge('contextSearchCall', 'reviewCall')
-      .compile();
-  }
-
-  async startReview(input: ReviewInput): Promise<ReviewOutput> {
-    const {
-      title,
-      commits, diff, editedFiles, description, sourceName, targetName, gatherContext, refreshCache,
-    } = input;
-
-    const response = await this.graph.invoke({
-      commits,
-      title,
-      description,
-      diff,
-      editedFiles,
-      messages: [],
-      gatherContext: gatherContext ?? true,
-      refreshCache: refreshCache ?? false,
-      sourceBranch: sourceName,
-      targetBranch: targetName,
-      sourceHash: input.sourceHash,
-      targetHash: input.targetHash,
-    });
-
-    return {
-      comments: response.comments,
-      context: response.context || '',
-      result: response.result || '',
-    };
-  }
-
   async *streamReview(input: ReviewInput): AsyncGenerator<ReviewOutput | NodeEvent> {
     const {
       title,
@@ -78,5 +41,14 @@ export class ReviewService {
     for await (const event of events) {
       yield event;
     }
+  }
+
+  private buildGraph() {
+    return new StateGraph(reviewState)
+      .addNode('contextSearchCall', (state: ReviewState, config: LangGraphRunnableConfig) => this.contextGatherer.gather(state, config))
+      .addNode('reviewCall', (state: ReviewState, config: LangGraphRunnableConfig) => this.codeReviewer.review(state,config))
+      .addEdge(START, 'contextSearchCall')
+      .addEdge('contextSearchCall', 'reviewCall')
+      .compile();
   }
 }
